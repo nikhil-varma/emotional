887b0b351dbc579e9cae4b6223b07862ea594e92
Nikhil Varma <nikhilvarma892@gmail.com>
2020-09-29 23:54:31 +0530

    fix: Create shared API file

--- src/shared/APIController.js
+++ src/shared/APIController.js
@@ -1,44 +1,14 @@
-import React, { Component } from "react";
-import ReactionButton from "./components/ReactionButton/ReactionButton";
-import Emoji from "a11y-react-emoji";
-import "./App.scss";
+export const getReactions = fetch(
+  "https://artful-iudex.herokuapp.com/reactions"
+).then((res) => res.json());
 
-class APIController extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reactions: [],
-    };
-  }
-  render() {
-    const { reactions } = this.state;
-    const popoverContentEmojis = (
-      <span className="flex justify-center">
-        {reactions.map((reaction) => {
-          const { emoji, id } = reaction;
-          return (
-            <span className="emoji-wrapper" key={id}>
-              {" "}
-              <Emoji
-                className="emoji"
-                symbol={emoji}
-                onClick={() => this.handleEmojiClick(id)}
-              />
-              <div className="info">{reaction.name}</div>
-            </span>
-          );
-        })}
-      </span>
-    );
-    return (
-      <div className="app-container">
-        <ReactionButton
-          popoverContent={popoverContentEmojis}
-          className="reaction-button"
-        />
-      </div>
-    );
-  }
-}
+export const getContentReactions = (content_id) =>
+  fetch(
+    `https://artful-iudex.herokuapp.com/user_content_reactions${
+      content_id ? `?content_id=${content_id}` : ""
+    }`
+  ).then((res) => res.json());
 
-export default APIController;
+export const getUsers = fetch(
+  "https://artful-iudex.herokuapp.com/users"
+).then((res) => res.json());
